61923e10eb866d49638de12b359d6ebe4011c289
Praveen P <hemapriyanp@gmail.com>
2026-04-14 15:26:42 +0530

    Fix README, login form and all changes

--- app.py
+++ app.py
@@ -581,4 +581,5 @@ def snapshot():
     return jsonify({"ok": False})
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=5000, threaded=True)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
